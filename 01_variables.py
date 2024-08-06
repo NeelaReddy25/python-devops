c0a184e71dd5bff13de6458cff4f5fb333957cb2
NeelaReddy <neelareddy.i25@gmail.com>
2024-08-06 23:53:32 +0530

    Types of variables

--- 01_variables.py
+++ 01_variables.py
@@ -5,6 +5,32 @@ multi-line comment
 """
 
 # Define a variable
-a=42
+a = 42 # This is a integer variable
 # print the value that is stored inside the variable
 print(a)
+
+"""
+- Integer
+- Boolean
+- Strings
+- Float
+"""
+
+# Float values or decimal values
+b = 42.345
+print(b)
+
+# Boolean
+c = True # true
+print(c)
+
+# Strings
+d = 'This is a string'
+e = "This is also a string"
+f = """
+This is a multi line string
+"""
+
+# Today's weather is nice
+d = 'Today's weather is nice'
+print(d)
