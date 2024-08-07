4e9a886712f50a43274537bb04e21ab250356f1c
NeelaReddy <neelareddy.i25@gmail.com>
2024-08-08 03:20:07 +0530

    Unpacking

--- 03_tuples.py
+++ 03_tuples.py
@@ -55,3 +55,6 @@ ValueError: tuple.index(x): x not in tuple
 # Tuple unpacking
 ansible, terraform, jenkins, docker, k8s = ("Ansible", "Terraform", "Jenkins", "Docker", "K8s")
 print(ansible, terraform, jenkins, docker, k8s)
+
+ansible, *tools, orchestrator = ("Ansible", "Terraform", "Jenkins", "Docker", "K8s", "DevOps")
+print(ansible, tools, orchestrator)
