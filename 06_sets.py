6c7a501017accff259b4794f638f101577c7010c
NeelaReddy <neelareddy.i25@gmail.com>
2024-08-08 18:22:02 +0530

    Add  element to set

--- 06_sets.py
+++ 06_sets.py
@@ -3,3 +3,8 @@ print(sample_set)
 
 # Set returns unique elememts that is stored insode that variable
 # Sets don't consider the order of insertion
+# Set also a mutable datatype
+
+sample_set = {1, 2, 5, 4, 2, 3}
+sample_set.add(6)
+print(sample_set)
