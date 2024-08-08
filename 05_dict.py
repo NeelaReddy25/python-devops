2892bb0bc706357bc99dedc97ea4d33f846fa2a5
NeelaReddy <neelareddy.i25@gmail.com>
2024-08-08 17:37:11 +0530

    Methods

--- 05_dict.py
+++ 05_dict.py
@@ -8,5 +8,20 @@ print(sample_dict[3])
 sample_dict = {(1, 2, 3, 4): 1, 2: 4, 3: 9}
 print(sample_dict[(1, 2, 3, 4)])
 
-sample_dict = {(1, 2, 3, 4): 1, 2: 4, 3: 9}
-print(sample_dict[[1, 2, 3, 4]])
+sample_dict = {"1": 1, 2: 4, 3: 9}
+print(sample_dict["1"])
+
+# sample_dict = {[1, 2, 3, 4]: 1, 2: 4, 3: 9}
+# print(sample_dict[[1, 2, 3, 4]])
+
+"""
+Traceback (most recent call last):
+  File "C:\devops\daws-78s\repos\python-devops\05_dict.py", line 12, in <module>
+    print(sample_dict[[1, 2, 3, 4]])
+          ~~~~~~~~~~~^^^^^^^^^^^^^^
+TypeError: unhashable type: 'list'
+"""
+dict_keys = sample_dict.keys()
+dict_values = sample_dict.values()
+dict_items = sample_dict.items()
+print(dict_keys, dict_values, dict_items)
