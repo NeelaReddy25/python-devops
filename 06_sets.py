3ed7b3396131706a91ba82a9abebbc12106539b9
NeelaReddy <neelareddy.i25@gmail.com>
2024-08-08 18:45:41 +0530

    Type casting

--- 06_sets.py
+++ 06_sets.py
@@ -9,6 +9,14 @@ print(sample_set)
 sample_set = {1, 2, 5, 4, 2, 3}
 sample_set.add(6)
 print(sample_set)
-print(sample_set[2])
+#print(sample_set[2])
+
+"""
+Traceback (most recent call last):
+  File "C:\devops\daws-78s\repos\python-devops\06_sets.py", line 12, in <module>
+    print(sample_set[2])
+          ~~~~~~~~~~^^^
+TypeError: 'set' object is not subscriptable
+"""
 
 # Intersection(), union()
