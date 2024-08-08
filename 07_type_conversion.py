d3b06fdec2a5ef6acfea65c384a472ac3635e3b8
NeelaReddy <neelareddy.i25@gmail.com>
2024-08-08 18:59:14 +0530

    User input conversion

--- 07_type_conversion.py
+++ 07_type_conversion.py
@@ -9,5 +9,16 @@ print(sample_str_tuple)
 user_input = input("Enter a number:")
 print(user_input, type(user_input))
 
-add_10 = user_input + 10
+# add_10 = user_input + 10
+# print(add_10)
+
+"""
+Traceback (most recent call last):
+  File "C:\devops\daws-78s\repos\python-devops\07_type_conversion.py", line 12, in <module>
+    add_10 = user_input + 10
+             ~~~~~~~~~~~^~~~
+TypeError: can only concatenate str (not "int") to str
+"""
+
+add_10 = int(user_input) + 10
 print(add_10)
