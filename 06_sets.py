21c1a3fb389876530d18eacb321ccd4a6afba1af
NeelaReddy <neelareddy.i25@gmail.com>
2024-08-08 18:29:19 +0530

    Access elements

--- 06_sets.py
+++ 06_sets.py
@@ -3,8 +3,12 @@ print(sample_set)
 
 # Set returns unique elememts that is stored insode that variable
 # Sets don't consider the order of insertion
-# Set also a mutable datatype
+# Set is also a mutable datatype
+# Sets don't support indexing
 
 sample_set = {1, 2, 5, 4, 2, 3}
 sample_set.add(6)
 print(sample_set)
+print(sample_set[2])
+
+# Intersection(), union()
