fc9ab93f9df03a15e082d3589cefad8a508d2d2f
NeelaReddy <neelareddy.i25@gmail.com>
2024-08-08 18:48:04 +0530

    Error type

--- 07_type_conversion.py
+++ 07_type_conversion.py
@@ -6,6 +6,6 @@ sample_str_tuple = tuple(sample_str)
 print(sample_str_tuple)
 
 # Accept input from a user
-user_input = input("Enter a number")
+user_input = input("Enter a number:")
 print(user_input, type(user_input))
 
