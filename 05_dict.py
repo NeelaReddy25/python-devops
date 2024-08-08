0ae153f925de61191c828b05ef81dafa49fe3ea4
NeelaReddy <neelareddy.i25@gmail.com>
2024-08-08 17:49:58 +0530

    Elements doesn't exist

--- 05_dict.py
+++ 05_dict.py
@@ -25,3 +25,8 @@ dict_keys = sample_dict.keys()
 dict_values = sample_dict.values()
 dict_items = sample_dict.items()
 print(dict_keys, dict_values, dict_items)
+
+# what happens if you access a key that is not present inside a dict
+sample_dict = {"1": 1, 2: 4, 3: 9}
+print(sample_dict.get(1)) # None
+print(sample_dict[1]) # Error
