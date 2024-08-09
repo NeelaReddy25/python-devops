2ba8b1a7c7439e4361f072583f3ee211090caf07
NeelaReddy <neelareddy.i25@gmail.com>
2024-08-09 16:46:22 +0530

    Range output

--- 10_loops.py
+++ 10_loops.py
@@ -20,3 +20,6 @@ sample_list = ["Ansible", "Terraform", "Jenkins", "Docker", "K8s"]
 
 sample_range = range(0, len(sample_list))
 print(sample_range, type(sample_range))
+
+for idx in sample_range:
+    print(idx)
