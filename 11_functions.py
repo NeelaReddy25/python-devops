af979ef77db3b78087786fdc8c41578bad170165
NeelaReddy <neelareddy.i25@gmail.com>
2024-08-09 17:27:57 +0530

    Default oparams

--- 11_functions.py
+++ 11_functions.py
@@ -12,15 +12,26 @@
 # res = add(1,2)
 # print(res)
 
-def add(num_1, num_2):
+# def add(num_1, num_2):
+#     """
+#     This function performs addition of 2 numbers
+#     """
+#     res = num_1 + num_2
+#     return res
+
+# # Call the function
+# res = add(num_2=1, num_1=2)
+# print(res)
+
+# help(add)
+
+def add(num_1, num_2, num_3=10):
     """
     This function performs addition of 2 numbers
     """
-    res = num_1 + num_2
+    res = num_1 + num_2 + num_3
     return res
 
 # Call the function
 res = add(num_2=1, num_1=2)
 print(res)
-
-help(add)
