63d91d5982ecdaaed43115c26c81a4bbf980c8c4
NeelaReddy <neelareddy.i25@gmail.com>
2024-08-09 17:24:30 +0530

    Help function

--- 11_functions.py
+++ 11_functions.py
@@ -4,10 +4,23 @@
 # # Call the function
 # sample_func()
 
+# def add(num_1, num_2):
+#     res = num_1 + num_2
+#     return res
+
+# # Call the function
+# res = add(1,2)
+# print(res)
+
 def add(num_1, num_2):
+    """
+    This function performs addition of 2 numbers
+    """
     res = num_1 + num_2
     return res
 
 # Call the function
-res = add(1,2)
+res = add(num_2=1, num_1=2)
 print(res)
+
+help(add)
